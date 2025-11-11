21f84bd4c77b8a822bc3d67b3ad7544164941193
Krees Sama <krees.sama@gmail.com>
2025-11-11 21:18:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ document.querySelectorAll('.has-sub .subbtn').forEach(btn=>{
 
 document.getElementById('year')?.append(new Date().getFullYear())
 
-// rendre le logo vers l accueil et stylisé premium
 document.querySelectorAll('.brand').forEach(a=>{
   a.addEventListener('mouseenter', ()=> a.style.filter='brightness(1.2)')
   a.addEventListener('mouseleave', ()=> a.style.filter='')
