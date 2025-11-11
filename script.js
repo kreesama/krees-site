b51af953bedd1f82837ad5781d8d76fb0df349f4
Krees Sama <krees.sama@gmail.com>
2025-11-11 21:45:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-document.querySelectorAll('.has-sub .subbtn').forEach(btn=>{
-  btn.addEventListener('click', e=>{
-    e.currentTarget.parentElement.classList.toggle('open')
-  })
-})
-
-document.getElementById('year')?.append(new Date().getFullYear())
-
-document.querySelectorAll('.brand').forEach(a=>{
-  a.addEventListener('mouseenter', ()=> a.style.filter='brightness(1.2)')
-  a.addEventListener('mouseleave', ()=> a.style.filter='')
-})
+console.log("Krees Sama – site loaded");
